9739060090ea07375a79d2bcb3353cf9907cf33b
Cyphle <cyril.phamle@gmail.com>
2025-09-04 16:17:08 +0200

    database

--- AWS/02-databases/variables.tf
+++ AWS/02-databases/variables.tf
@@ -1,4 +1,20 @@
-# Variables for Databases Module
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "navigator"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
 
 variable "name_prefix" {
   description = "Name prefix for resources"
@@ -30,4 +46,13 @@ variable "database_subnet_group_name" {
 variable "database_security_group_id" {
   description = "ID of the database security group"
   type        = string
+}
+
+locals {
+  name_prefix = "${var.project_name}-${var.environment}"
+  common_tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
 }
