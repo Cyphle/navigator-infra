e730b04d27eb57a02cbfa9dd847b6177a081e7e4
Cyphle <cyril.phamle@gmail.com>
2025-09-12 15:49:40 +0200

    alb

--- infra/04-alb/outputs.tf
+++ infra/04-alb/outputs.tf
@@ -6,37 +6,22 @@ output "alb_security_group_id" {
 
 output "alb_id" {
   description = "ID of the Application Load Balancer"
-  value       = aws_lb.main.id
+  value       = aws_alb.main.id
 }
 
 output "alb_arn" {
   description = "ARN of the Application Load Balancer"
-  value       = aws_lb.main.arn
+  value       = aws_alb.main.arn
 }
 
 output "alb_dns_name" {
   description = "DNS name of the Application Load Balancer"
-  value       = aws_lb.main.dns_name
+  value       = aws_alb.main.dns_name
 }
 
 output "alb_zone_id" {
   description = "Zone ID of the Application Load Balancer"
-  value       = aws_lb.main.zone_id
-}
-
-output "frontend_target_group_arn" {
-  description = "ARN of the frontend target group"
-  value       = aws_lb_target_group.frontend.arn
-}
-
-output "backend_target_group_arn" {
-  description = "ARN of the backend target group"
-  value       = aws_lb_target_group.backend.arn
-}
-
-output "keycloak_target_group_arn" {
-  description = "ARN of the keycloak target group"
-  value       = aws_lb_target_group.keycloak.arn
+  value       = aws_alb.main.zone_id
 }
 
 output "certificate_arn" {
--- infra/04-alb/variables.tf
+++ infra/04-alb/variables.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  description = "Default AWS region name"
+  type        = string
+  default     = "eu-west-3"
+}
+
 variable "project_name" {
   description = "Name of the project"
   type        = string
