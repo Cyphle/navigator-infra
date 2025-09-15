8810331835a36fc9a7a8d0cb6e66daaf7eb6dcd6
Cyphle <cyril.phamle@gmail.com>
2025-09-15 14:11:47 +0200

    comment vpc endpoints

--- infra/01-network/outputs.tf
+++ infra/01-network/outputs.tf
@@ -29,37 +29,6 @@ output "database_subnet_group_name" {
   value       = aws_db_subnet_group.main.name
 }
 
-# VPC Endpoints outputs
-output "vpc_endpoints_security_group_id" {
-  description = "ID of the VPC endpoints security group"
-  value       = aws_security_group.vpc_endpoints.id
-}
-
-output "ecr_api_endpoint_id" {
-  description = "ID of the ECR API VPC endpoint"
-  value       = aws_vpc_endpoint.ecr_api.id
-}
-
-output "ecr_dkr_endpoint_id" {
-  description = "ID of the ECR DKR VPC endpoint"
-  value       = aws_vpc_endpoint.ecr_dkr.id
-}
-
-output "logs_endpoint_id" {
-  description = "ID of the CloudWatch Logs VPC endpoint"
-  value       = aws_vpc_endpoint.logs.id
-}
-
-output "secretsmanager_endpoint_id" {
-  description = "ID of the Secrets Manager VPC endpoint"
-  value       = aws_vpc_endpoint.secretsmanager.id
-}
-
-output "s3_endpoint_id" {
-  description = "ID of the S3 Gateway VPC endpoint"
-  value       = aws_vpc_endpoint.s3.id
-}
-
 # NAT Gateway outputs
 output "nat_gateway_ids" {
   description = "IDs of the NAT Gateways"
@@ -70,3 +39,35 @@ output "nat_gateway_public_ips" {
   description = "Public IPs of the NAT Gateways"
   value       = aws_eip.nat[*].public_ip
 }
+
+# TODO: THIS IS NOT CHEAP!!! DEPLOY WITH CAUTION!!!
+# # VPC Endpoints outputs
+# output "vpc_endpoints_security_group_id" {
+#   description = "ID of the VPC endpoints security group"
+#   value       = aws_security_group.vpc_endpoints.id
+# }
+#
+# output "ecr_api_endpoint_id" {
+#   description = "ID of the ECR API VPC endpoint"
+#   value       = aws_vpc_endpoint.ecr_api.id
+# }
+#
+# output "ecr_dkr_endpoint_id" {
+#   description = "ID of the ECR DKR VPC endpoint"
+#   value       = aws_vpc_endpoint.ecr_dkr.id
+# }
+#
+# output "logs_endpoint_id" {
+#   description = "ID of the CloudWatch Logs VPC endpoint"
+#   value       = aws_vpc_endpoint.logs.id
+# }
+#
+# output "secretsmanager_endpoint_id" {
+#   description = "ID of the Secrets Manager VPC endpoint"
+#   value       = aws_vpc_endpoint.secretsmanager.id
+# }
+#
+# output "s3_endpoint_id" {
+#   description = "ID of the S3 Gateway VPC endpoint"
+#   value       = aws_vpc_endpoint.s3.id
+# }
--- infra/01-network/vpc-endpoints.tf
+++ infra/01-network/vpc-endpoints.tf
@@ -1,247 +1,249 @@
-# VPC Endpoints Configuration
-# Replaces NAT Gateway for AWS services access
+# TODO: THIS IS NOT CHEAP!!! DEPLOY WITH CAUTION!!!
 
-# Security Group for VPC Endpoints
-resource "aws_security_group" "vpc_endpoints" {
-  name_prefix = "${var.project_name}-vpc-endpoints-"
-  vpc_id      = aws_vpc.main.id
-  description = "Security group for VPC endpoints"
-
-  ingress {
-    description = "HTTPS from private subnets"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [for subnet in aws_subnet.private : subnet.cidr_block]
-  }
-
-  egress {
-    description = "All outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(local.common_tags, {
-    Name = "${var.project_name}-vpc-endpoints-sg"
-  })
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# ECR API Endpoint (for ECS task definitions)
-resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${var.aws_region}.ecr.api"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.private[*].id
-  security_group_ids  = [aws_security_group.vpc_endpoints.id]
-  private_dns_enabled = true
-  
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-
-  tags = merge(local.common_tags, {
-    Name = "${var.project_name}-ecr-api-endpoint"
-  })
-}
-
-# ECR DKR Endpoint (for container image pulls)
-resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${var.aws_region}.ecr.dkr"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.private[*].id
-  security_group_ids  = [aws_security_group.vpc_endpoints.id]
-  private_dns_enabled = true
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-
-  tags = merge(local.common_tags, {
-    Name = "${var.project_name}-ecr-dkr-endpoint"
-  })
-}
-
-# CloudWatch Logs Endpoint (for ECS logging)
-resource "aws_vpc_endpoint" "logs" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${var.aws_region}.logs"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.private[*].id
-  security_group_ids  = [aws_security_group.vpc_endpoints.id]
-  private_dns_enabled = true
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "logs:DescribeLogStreams"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-
-  tags = merge(local.common_tags, {
-    Name = "${var.project_name}-logs-endpoint"
-  })
-}
-
-# Secrets Manager Endpoint (for database and Keycloak credentials)
-resource "aws_vpc_endpoint" "secretsmanager" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${var.aws_region}.secretsmanager"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.private[*].id
-  security_group_ids  = [aws_security_group.vpc_endpoints.id]
-  private_dns_enabled = true
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = [
-          "secretsmanager:GetSecretValue",
-          "secretsmanager:DescribeSecret"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-
-  tags = merge(local.common_tags, {
-    Name = "${var.project_name}-secretsmanager-endpoint"
-  })
-}
-
-# S3 Gateway Endpoint (FREE - for ECR layer storage and general S3 access)
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${var.aws_region}.s3"
-  vpc_endpoint_type = "Gateway"
-  route_table_ids   = aws_route_table.private[*].id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          "arn:aws:s3:::*"
-        ]
-      }
-    ]
-  })
-
-  tags = merge(local.common_tags, {
-    Name = "${var.project_name}-s3-endpoint"
-  })
-}
-
-# ECS Endpoint (for ECS API calls)
-resource "aws_vpc_endpoint" "ecs" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${var.aws_region}.ecs"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.private[*].id
-  security_group_ids  = [aws_security_group.vpc_endpoints.id]
-  private_dns_enabled = true
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = [
-          "ecs:CreateCluster",
-          "ecs:DescribeClusters",
-          "ecs:DescribeServices",
-          "ecs:DescribeTasks",
-          "ecs:ListTasks",
-          "ecs:UpdateService"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-
-  tags = merge(local.common_tags, {
-    Name = "${var.project_name}-ecs-endpoint"
-  })
-}
-
-# ECS Agent Endpoint (for ECS agent communication)
-resource "aws_vpc_endpoint" "ecs_agent" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${var.aws_region}.ecs-agent"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.private[*].id
-  security_group_ids  = [aws_security_group.vpc_endpoints.id]
-  private_dns_enabled = true
-
-  tags = merge(local.common_tags, {
-    Name = "${var.project_name}-ecs-agent-endpoint"
-  })
-}
-
-# ECS Telemetry Endpoint (for ECS telemetry)
-resource "aws_vpc_endpoint" "ecs_telemetry" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${var.aws_region}.ecs-telemetry"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.private[*].id
-  security_group_ids  = [aws_security_group.vpc_endpoints.id]
-  private_dns_enabled = true
-
-  tags = merge(local.common_tags, {
-    Name = "${var.project_name}-ecs-telemetry-endpoint"
-  })
-}
+# # VPC Endpoints Configuration
+# # Replaces NAT Gateway for AWS services access
+#
+# # Security Group for VPC Endpoints
+# resource "aws_security_group" "vpc_endpoints" {
+#   name_prefix = "${var.project_name}-vpc-endpoints-"
+#   vpc_id      = aws_vpc.main.id
+#   description = "Security group for VPC endpoints"
+#
+#   ingress {
+#     description = "HTTPS from private subnets"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = [for subnet in aws_subnet.private : subnet.cidr_block]
+#   }
+#
+#   egress {
+#     description = "All outbound traffic"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   tags = merge(local.common_tags, {
+#     Name = "${var.project_name}-vpc-endpoints-sg"
+#   })
+#
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+#
+# # ECR API Endpoint (for ECS task definitions)
+# resource "aws_vpc_endpoint" "ecr_api" {
+#   vpc_id              = aws_vpc.main.id
+#   service_name        = "com.amazonaws.${var.aws_region}.ecr.api"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = aws_subnet.private[*].id
+#   security_group_ids  = [aws_security_group.vpc_endpoints.id]
+#   private_dns_enabled = true
+#
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = "*"
+#         Action = [
+#           "ecr:GetAuthorizationToken",
+#           "ecr:BatchCheckLayerAvailability",
+#           "ecr:GetDownloadUrlForLayer",
+#           "ecr:BatchGetImage"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+#
+#   tags = merge(local.common_tags, {
+#     Name = "${var.project_name}-ecr-api-endpoint"
+#   })
+# }
+#
+# # ECR DKR Endpoint (for container image pulls)
+# resource "aws_vpc_endpoint" "ecr_dkr" {
+#   vpc_id              = aws_vpc.main.id
+#   service_name        = "com.amazonaws.${var.aws_region}.ecr.dkr"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = aws_subnet.private[*].id
+#   security_group_ids  = [aws_security_group.vpc_endpoints.id]
+#   private_dns_enabled = true
+#
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = "*"
+#         Action = [
+#           "ecr:GetAuthorizationToken",
+#           "ecr:BatchCheckLayerAvailability",
+#           "ecr:GetDownloadUrlForLayer",
+#           "ecr:BatchGetImage"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+#
+#   tags = merge(local.common_tags, {
+#     Name = "${var.project_name}-ecr-dkr-endpoint"
+#   })
+# }
+#
+# # CloudWatch Logs Endpoint (for ECS logging)
+# resource "aws_vpc_endpoint" "logs" {
+#   vpc_id              = aws_vpc.main.id
+#   service_name        = "com.amazonaws.${var.aws_region}.logs"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = aws_subnet.private[*].id
+#   security_group_ids  = [aws_security_group.vpc_endpoints.id]
+#   private_dns_enabled = true
+#
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = "*"
+#         Action = [
+#           "logs:CreateLogGroup",
+#           "logs:CreateLogStream",
+#           "logs:PutLogEvents",
+#           "logs:DescribeLogStreams"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+#
+#   tags = merge(local.common_tags, {
+#     Name = "${var.project_name}-logs-endpoint"
+#   })
+# }
+#
+# # Secrets Manager Endpoint (for database and Keycloak credentials)
+# resource "aws_vpc_endpoint" "secretsmanager" {
+#   vpc_id              = aws_vpc.main.id
+#   service_name        = "com.amazonaws.${var.aws_region}.secretsmanager"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = aws_subnet.private[*].id
+#   security_group_ids  = [aws_security_group.vpc_endpoints.id]
+#   private_dns_enabled = true
+#
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = "*"
+#         Action = [
+#           "secretsmanager:GetSecretValue",
+#           "secretsmanager:DescribeSecret"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+#
+#   tags = merge(local.common_tags, {
+#     Name = "${var.project_name}-secretsmanager-endpoint"
+#   })
+# }
+#
+# # S3 Gateway Endpoint (FREE - for ECR layer storage and general S3 access)
+# resource "aws_vpc_endpoint" "s3" {
+#   vpc_id            = aws_vpc.main.id
+#   service_name      = "com.amazonaws.${var.aws_region}.s3"
+#   vpc_endpoint_type = "Gateway"
+#   route_table_ids   = aws_route_table.private[*].id
+#
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = "*"
+#         Action = [
+#           "s3:GetObject",
+#           "s3:PutObject",
+#           "s3:DeleteObject",
+#           "s3:ListBucket"
+#         ]
+#         Resource = [
+#           "arn:aws:s3:::*"
+#         ]
+#       }
+#     ]
+#   })
+#
+#   tags = merge(local.common_tags, {
+#     Name = "${var.project_name}-s3-endpoint"
+#   })
+# }
+#
+# # ECS Endpoint (for ECS API calls)
+# resource "aws_vpc_endpoint" "ecs" {
+#   vpc_id              = aws_vpc.main.id
+#   service_name        = "com.amazonaws.${var.aws_region}.ecs"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = aws_subnet.private[*].id
+#   security_group_ids  = [aws_security_group.vpc_endpoints.id]
+#   private_dns_enabled = true
+#
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = "*"
+#         Action = [
+#           "ecs:CreateCluster",
+#           "ecs:DescribeClusters",
+#           "ecs:DescribeServices",
+#           "ecs:DescribeTasks",
+#           "ecs:ListTasks",
+#           "ecs:UpdateService"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+#
+#   tags = merge(local.common_tags, {
+#     Name = "${var.project_name}-ecs-endpoint"
+#   })
+# }
+#
+# # ECS Agent Endpoint (for ECS agent communication)
+# resource "aws_vpc_endpoint" "ecs_agent" {
+#   vpc_id              = aws_vpc.main.id
+#   service_name        = "com.amazonaws.${var.aws_region}.ecs-agent"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = aws_subnet.private[*].id
+#   security_group_ids  = [aws_security_group.vpc_endpoints.id]
+#   private_dns_enabled = true
+#
+#   tags = merge(local.common_tags, {
+#     Name = "${var.project_name}-ecs-agent-endpoint"
+#   })
+# }
+#
+# # ECS Telemetry Endpoint (for ECS telemetry)
+# resource "aws_vpc_endpoint" "ecs_telemetry" {
+#   vpc_id              = aws_vpc.main.id
+#   service_name        = "com.amazonaws.${var.aws_region}.ecs-telemetry"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = aws_subnet.private[*].id
+#   security_group_ids  = [aws_security_group.vpc_endpoints.id]
+#   private_dns_enabled = true
+#
+#   tags = merge(local.common_tags, {
+#     Name = "${var.project_name}-ecs-telemetry-endpoint"
+#   })
+# }
